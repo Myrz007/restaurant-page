cd2c42a5cd96192d1fcda1f98d61e662ba98d283
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2024-04-16 16:33:23 +0200

    Add selector variables

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const home = document.querySelector('#home');
+const menus = document.querySelector('#menus');
+const contact = document.querySelector('#contact');
+const content = document.querySelector('#content');
