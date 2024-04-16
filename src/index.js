17cad5da521e17bc364a805c67e4cef2fda6a792
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2024-04-16 16:45:31 +0200

    Add module imports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import homeComponent from "./home.js";
+import menusComponent from "./menus.js";
+import contactComponent from "./contact.js";
+
 const home = document.querySelector('#home');
 const menus = document.querySelector('#menus');
 const contact = document.querySelector('#contact');
