bf1925cb1287b100d860485ef69a039a2f342031
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2024-04-19 15:20:01 +0200

    Add homeComponent function

---
+++ src/home.js
@@ -0,0 +1,14 @@
+export default function homeComponent() {
+    const mainElement = document.createElement('div');
+    const title = document.createElement('h1');
+    const description = document.createElement('p');
+    const image = document.createElement('img');
+
+    title.innerHTML = 'Barney\'s Tavern';
+    description.innerHTML = 'Welcome to Barney\'s Tavern! All we serve is elaborated with fresh ingredients collected from our trustworthy local partners. Just come, relax and enjoy a great time!';
+    image.src = './images/tavern.jpg';
+    
+    mainElement.append(title, description, image);
+
+    return mainElement;
+}
