79386ca81930059dd9cc080d201acac7e3e8dcba
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2024-04-24 16:35:21 +0200

    Set mainElement id to contact-content

--- src/contact.js
+++ src/contact.js
@@ -2,12 +2,13 @@ export default function contactComponent() {
     const mainElement = document.createElement('div');
     const title = document.createElement('h1');
     const address = document.createElement('p');
-    const phoneNumber = document.createElement('p')
+    const phoneNumber = document.createElement('p');
 
     title.innerHTML = 'Contact Us';
     address.innerHTML = 'Barney\'s Tavern<br>123 Somewhere Street<br>Somewhere around the world and maybe closer than you think';
     phoneNumber.innerHTML = 'Call +01 1234567890';
     
+    mainElement.setAttribute('id', 'contact-content');
     mainElement.append(title, address, phoneNumber);
 
     return mainElement;
